8e67fee8adcfb30898bb67527e01ff6aa80c315a
bidus92 <bidusblogs@gmail.com>
2024-02-16 12:58:08 -0500

    Begin implementation of database. Needs work. TODO: implement MD array to handle data that requires such. Implement combination of raw promise based system with async/await to acquire information and to be sure that all present information accessed from the API is accounted for

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express"; 
 import bodyParser from "body-parser"; 
-import axios from "axios"; 
+import axios, { all } from "axios"; 
 import pg from "pg"; 
 import ejs from "ejs"; 
 
@@ -22,6 +22,31 @@ const db = new pg.Client({
 
 
 db.connect(); 
+
+function bookCoverRejected(err)
+{
+    console.log("There is no book cover here :("); 
+    console.log(err);
+}
+//promsise that url returns if one is found, but if status code 404 is returned it is rejected
+async function checkBookCover(bookCoverLink)
+{ 
+    var bookCover = await axios.get(bookCoverLink);
+    return new Promise(function(resolve, reject)
+    {
+        if(bookCover === 404)
+        {
+            reject("No book cover exists");
+        }
+        else
+        {
+            // console.log(bookCover.config.url);
+            resolve(bookCover.config.url); 
+        }
+    });
+}
+
+
 
 
 app.get("/", async (req, res) =>
@@ -46,9 +71,113 @@ app.get("/reviews", (req, res) =>
     });
 });
 
+// TODO: Find check for image existence 
+//and if none to skip that imageURL, create loop in EJS for database to loop through authors
+//Create loop to limit search results to 10 per page and then create a next page for search results and continue 
+//the number
+
+app.post("/database", async (req, res) =>
+{
+    const rawTitle = req.body.title.toLowerCase(); 
+    // console.log(rawTitle);
+    // console.log(typeof(rawTitle));
+
+
+    const title = rawTitle.replaceAll(" ", "+"); 
+
+
+    // console.log(title);
+    const result = await axios.get(`https://openlibrary.org/search.json?title=${title}`);
+    
+    var searchResults = result.data.docs;
+
+    //store chosen book covers
+    var chosenBookCovers = [];
+    var theTitles = [];
+
+    var allAuthors = [[]];
+    //acquire documents
+    
+    
+    //loop through results, take title of each result and push authors into an array to list accordingly
+   for(let x = 0; x < searchResults.length; x++)
+    {
+        theTitles.push(searchResults[x].title); 
+        var theAuthors = []; 
+        //loop through authors to push to array to ultimately display
+        if(searchResults[x].author_name)
+        {
+            for(let y = 0; y < searchResults[x].author_name.length; y++)
+            {
+                theAuthors.push(searchResults[x].author_name[y]);
+            }
+
+            allAuthors.push(theAuthors); 
+        }
+        else
+        {
+            theAuthors.push(["N/A"]);
+        }
+        
+        // array of promises to weed out which ones are good to use and which ones get rejected
+        const bookCoverPromises = []; 
+        //bool to determine if a book cover was found
+        var found = false; 
+        //loop through isbn numbers to get the promises to push to promises array to get a cover
+        if(searchResults[x].isbn)
+        {
+            for(let z = 0; z < searchResults[x].isbn.length; z++)
+            {
+                const bookCoverPromise = checkBookCover(`https://covers.openlibrary.org/b/isbn/${searchResults[x].isbn[z]}-M.jpg?default=false`);
+                bookCoverPromises.push(bookCoverPromise);
+            }
+        
+            const allPotentialCovers = Promise.allSettled(bookCoverPromises); 
+          
+            await allPotentialCovers.then(results =>
+                {
+                   
+                    // console.log(results);
+                    for(let w = 0; w < results.length; w++)
+                    {
+                        if(results[w].status === "rejected")
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            found = true; 
+                            chosenBookCovers.push(results[w].value);
+                            console.log("The chosen book cover is " + chosenBookCovers[w]);
+                            break;
+                        }
+                    }
+                });
+        }
+        if(!found)
+        {
+            chosenBookCovers.push("assets/images/not_available.jpg");
+        }
+    }
+
+   console.log(chosenBookCovers.length);
+   console.log(allAuthors);
+
+    res.render("database.ejs", 
+    {
+        resultNumber: searchResults.length, 
+        titles: theTitles,
+        authors: allAuthors, 
+        imgURLs: chosenBookCovers
+    });
+});
+
+
+
+
 app.get("/database", (req, res) =>
 {
-    res.render("search.ejs", 
+    res.render("database.ejs", 
     {
 
     });
